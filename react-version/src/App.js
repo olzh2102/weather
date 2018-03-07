ffc8a141acebfc3ae52d4efc0fe1d33f8fc0757f
olzhas2102 <olzhas2102@yahoo.com>
2018-03-07 16:26:29 +0100

    Displaying the weather on UI

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -8,13 +8,34 @@ const API_KEY = "99dbe65f9b8c7fb33a5eb3cb94684501";
 
 
 class App extends React.Component {  // creates instance of App that extends React.Component
+	state = {
+		temperature: undefined,
+		city: undefined,
+		country: undefined,
+		humidity: undefined,
+		description: undefined,
+		error: undefined
+	}
+
 	getWeather = async (e) => {
 		e.preventDefault();
 
-		const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Liverpool,uk&appid=${API_KEY}`);
+		const city = e.target.elements.city.value;
+		const country = e.target.elements.country.value;
+
+		const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
 
 		const responseData = await apiCall.json();
 		console.log(responseData);
+
+		this.setState({
+			temperature: responseData.main.temp,
+			city: responseData.name,
+			country: responseData.sys.country,
+			humidity: responseData.main.humidity,
+			description: responseData.weather[0].description,
+			error: ""
+		});
 	}
 
 	render() {
@@ -22,7 +43,14 @@ class App extends React.Component {  // creates instance of App that extends Rea
 			<div>
 				<Titles />
 				<Form getWeather={this.getWeather}/> {/* setting props to call getweather function */}
-				<Weather />
+				<Weather
+					temperature={this.state.temperature}
+					city={this.state.city}
+					country={this.state.country}
+					humidity={this.state.humidity}
+					description={this.state.description}
+					error={this.state.error}
+				/>
 			</div>
 		);
 	}
