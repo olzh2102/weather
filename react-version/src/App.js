ebe0bbe3d56aaf10788c49e99745754ca640d477
olzhas2102 <olzhas2102@yahoo.com>
2018-03-07 15:52:57 +0100

    Setting props to call function and fetching weather from API

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -4,12 +4,24 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY = "99dbe65f9b8c7fb33a5eb3cb94684501";
+
+
 class App extends React.Component {  // creates instance of App that extends React.Component
+	getWeather = async (e) => {
+		e.preventDefault();
+
+		const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Liverpool,uk&appid=${API_KEY}`);
+
+		const responseData = await apiCall.json();
+		console.log(responseData);
+	}
+
 	render() {
 		return (
 			<div>
 				<Titles />
-				<Form />
+				<Form getWeather={this.getWeather}/> {/* setting props to call getweather function */}
 				<Weather />
 			</div>
 		);
