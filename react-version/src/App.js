0799f8b45a5f9884d0c8f0d6e8e892e1769d283f
olzhas2102 <olzhas2102@yahoo.com>
2018-03-07 15:20:48 +0100

    Components

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -1,11 +1,16 @@
 import React from "react"; // importing React object from react package inside package.json
 
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
 class App extends React.Component {  // creates instance of App that extends React.Component
 	render() {
 		return (
 			<div>
-				<p>Hello!</p>
-				<h1>YO!</h1>
+				<Titles />
+				<Form />
+				<Weather />
 			</div>
 		);
 	}
