f74e8b1a251185a3405b0d3e7b21197a496989d0
olzhas2102 <olzhas2102@yahoo.com>
2018-03-07 10:56:59 +0100

    Save to local storage

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
+// Init storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
+
 // Init weather object
-const weather = new Weather('Boston', 'MA');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 // Init UI
 const ui = new UI();
 
 // Get weather on DOM load
-document.addEventListener('DOMContentLoaded', getWeather);
+document.addEventListener('DOMContentLoaded', setWeather);
+
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (event) => {
+	const city = document.getElementById('city').value;
+	const state = document.getElementById('state').value;
+
+	// Change location in LS
+	weather.changeLocation(city, state);
+
+	//Set locaiton in LS
+	storage.setLocationData(city, state);
+
+
+	// Get and display weather
+	setWeather();
+
+	// Close modal
+	$('#locModal').modal('hide');
+});
 
 //weather.changeLocation('Miami', 'FL');
 
-function getWeather() {
+function setWeather() {
 	weather.getWeather()
 		.then(results => {
 			ui.paint(results);
